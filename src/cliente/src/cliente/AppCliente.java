0add9f0ff72434d24441f217053b41de22bdbc42
Nicolás González <nikolasgonzalez8@gmail.com>
2017-07-03 02:14:44 -0400

    Commit inicial de la tarea 2 de redes 2017 - 01

---
+++ src/cliente/src/cliente/AppCliente.java
@@ -0,0 +1,11 @@
+package cliente;
+
+/**
+ * Created by Nicolas on 00002, 02-07-2017.
+ */
+public class AppCliente {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/servidor/src/servidor/AppServidor.java
@@ -0,0 +1,11 @@
+package servidor;
+
+/**
+ * Created by Nicolas on 00002, 02-07-2017.
+ */
+public class AppServidor {
+
+    public static void main(String[] args){
+
+    }
+}
