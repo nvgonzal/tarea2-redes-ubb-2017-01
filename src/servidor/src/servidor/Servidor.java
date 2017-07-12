aaf32fc748cf6afc324bebbed63db680a3be1f19
Nicolas Victor <nvgonzal@alumnos.ubiobio.cl>
2017-07-12 18:35:36 -0400

    Agrega mesajes de control

--- src/servidor/src/servidor/Servidor.java
+++ src/servidor/src/servidor/Servidor.java
@@ -40,33 +40,33 @@ public class Servidor {
     private void crearServidorEscucha(int puerto){
         ServerSocket s;
         try{
-            //TerminalLogger.TLog("Creando socket de escucha.",TerminalLogger.CONTROL);
+            TerminalLogger.TLog("Creando socket de escucha.",TerminalLogger.CONTROL);
             s = new ServerSocket(puerto);
             this.socketEscucha = s;
-            //TerminalLogger.TLog("Socket de escucha creado.",TerminalLogger.CONTROL);
+            TerminalLogger.TLog("Socket de escucha creado.",TerminalLogger.CONTROL);
         }
         catch (IOException e) {
-            //TerminalLogger.TLog("Error al crear el socket de escucha: "+e.toString(),TerminalLogger.ERR);
+            TerminalLogger.TLog("Error al crear el socket de escucha: "+e.toString(),TerminalLogger.ERR);
             System.exit(-1);
         }
 
     }
 
     public void escuchar(){
-        //TerminalLogger.TLog("Servidor escuchando en el puerto: "+puerto,TerminalLogger.CONTROL);
-        //TerminalLogger.TLog("Que tengas un buen dia.",TerminalLogger.CONTROL);
+        TerminalLogger.TLog("Servidor escuchando en el puerto: "+puerto,TerminalLogger.CONTROL);
+        TerminalLogger.TLog("Que tengas un buen dia.",TerminalLogger.CONTROL);
         while (true){
             try {
                 Socket conCliente = this.socketEscucha.accept();
-                //TerminalLogger.TLog("Conexion establecida con: "+
-                //        conCliente.getInetAddress().toString(),TerminalLogger.CONN);
+                TerminalLogger.TLog("Conexion establecida con: "+
+                        conCliente.getInetAddress().toString(),TerminalLogger.CONN);
                 ServidorTransmision a = new ServidorTransmision(conCliente);
                 hilosDeConexion.add(a);
                 a.start();
             }
             catch (IOException e){
-                //TerminalLogger.TLog("Error al crear la conexion con el cliente: "+e.toString()
-                  //      ,TerminalLogger.ERR);
+                TerminalLogger.TLog("Error al crear la conexion con el cliente: "+e.toString()
+                      ,TerminalLogger.ERR);
             }
         }
     }
