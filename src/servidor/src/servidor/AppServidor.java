cbe1de6d7bb12cbcb4811fb0b4bc14744f568828
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2017-07-19 00:35:09 -0400

    Agrega opcion al usuario del servidor de definir otro puerto

--- src/servidor/src/servidor/AppServidor.java
+++ src/servidor/src/servidor/AppServidor.java
@@ -6,7 +6,23 @@ package servidor;
 public class AppServidor {
 
     public static void main(String[] args){
-        Servidor servidor = new Servidor();
-        servidor.escuchar();
+        Servidor servidor;
+        switch (args.length){
+            case 0:
+                servidor = new Servidor();
+                servidor.escuchar();
+                break;
+            case 1:
+                int puerto = 0;
+                try {
+                    puerto = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    System.out.println("El puerto solo puede ser un numero");
+                    System.exit(-1);
+                }
+                servidor = new Servidor(puerto);
+                servidor.escuchar();
+                break;
+        }
     }
 }
